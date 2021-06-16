69cc1798dcf026890117d57fbd791acf06450dc6
sam-eee <sam-eee@github.com>
2021-06-16 10:26:35 +0100

    added lidar type

--- bacchus_bringup/tmule/robots/thorvald_008.sh
+++ bacchus_bringup/tmule/robots/thorvald_008.sh
@@ -6,7 +6,7 @@ export CAN_INTERFACE_NAME="/dev/ttyUSB0"
 #export USE_XSENS=true
 export LIDAR_TYPE_FRONT=nanoscan3
 export LIDAR_TYPE_BACK=nanoscan3
-
+export LIDAR_TYPE_TOP=os1
 USE_GPS_ONLY=true
 USE_MEL=true
 USE_RTK=true
